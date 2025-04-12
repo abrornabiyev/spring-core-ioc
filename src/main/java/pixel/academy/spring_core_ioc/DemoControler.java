fbce1c5254cbf8cf852bd9728d9017cf71a58c15
Пользователь <abror.nabiyev321@gmail.com>
2025-04-12 17:36:03 +0300

    Commit

---
+++ src/main/java/pixel/academy/spring_core_ioc/DemoControler.java
@@ -0,0 +1,21 @@
+package pixel.academy.spring_core_ioc;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoControler {
+
+    private Chef myChef;
+
+    @Autowired
+    public DemoControler(Chef theChef) {
+        myChef = theChef;
+    }
+
+    @GetMapping("/dailyrecipe")
+    public String getDailyRecipe() {
+        return myChef.getDailyRecipe();
+    }
+}
